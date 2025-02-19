8f0f21b42b9e078fa182da61a8f42daa2c7c61a7
unknown <otabecktursunov@gmail.com>
2025-02-19 14:42:02 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-45k(wu5+^(r5eib^489)#k1sm24)=s&6fw+(-l&@y_7ld2##2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
